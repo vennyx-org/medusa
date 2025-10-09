074b93be52592521fc417b5880ce74ec9c79ddfe
Faruk Aydoğan <aydoganf@itu.edu.tr>
2025-10-09 16:31:10 +0300

    fix(tax): change setTotalField parameter to undefined in calculateTaxTotal call
    Updated the `calculateAmountsWithTax` function to set the `setTotalField` parameter to `undefined` when calling `calculateTaxTotal`. This change improves the clarity of the function's intent and ensures proper handling of the parameter in tax calculations.

--- packages/core/utils/src/totals/tax/index.ts
+++ packages/core/utils/src/totals/tax/index.ts
@@ -105,7 +105,7 @@ export function calculateAmountsWithTax({
     const tax = calculateTaxTotal({
       taxLines,
       taxableAmount,
-      setTotalField: "",
+      setTotalField: undefined,
       roundHalfUp: true,
     })
     
